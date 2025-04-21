cc59cb9e3048d50562a5ba2f5f79ef50aa693cee
mat3535 <mat.a.vargas35@gmail.com>
2025-04-20 22:00:51 -0500

    Create rainingcatsdogs.py+creating basic loop and layout using pygame

---
+++ rainingcatsdogs.py
@@ -0,0 +1,36 @@
+import pygame
+from sys import exit
+
+
+pygame.init()
+screen =  pygame.display.set_mode((1000,800))
+clock = pygame.time.Clock()
+
+# Control variables
+# Top and ground
+top = 0
+ground = 700
+
+# game on/off
+rungame = True
+
+# Background graphics
+ground_s = pygame.image.load('graphics/floor.png').convert()
+ground_r = ground_s.get_rect(midtop = (500,550))
+back_s = pygame.image.load('graphics/top.png').convert()
+
+# Game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    if rungame:
+
+        # Draw background graphics
+        screen.blit(ground_s,(0,ground))
+        screen.blit(back_s,(0,0))
+
+    pygame.display.update()
+    clock.tick(60)
