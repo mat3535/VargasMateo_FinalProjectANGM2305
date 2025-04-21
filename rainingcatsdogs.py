c5062d701a222aa82c5c28a2ee8ef43e40b93a21
Mateo Vargas <mat.a.vargas35@gmail.com>
2025-04-21 18:59:39 -0500

    Introduces Player character, standing still

--- rainingcatsdogs.py
+++ rainingcatsdogs.py
@@ -19,6 +19,11 @@ ground_s = pygame.image.load('graphics/floor.png').convert()
 ground_r = ground_s.get_rect(midtop = (500,550))
 back_s = pygame.image.load('graphics/top.png').convert()
 
+# Player  graphics
+player_s = pygame.image.load('graphics/Player/stand_boy.png').convert_alpha()
+player_r = player_s.get_rect(midbottom = (500,ground))
+
+
 # Game loop
 while True:
     for event in pygame.event.get():
@@ -31,6 +36,8 @@ while True:
         # Draw background graphics
         screen.blit(ground_s,(0,ground))
         screen.blit(back_s,(0,0))
+
+        screen.blit(player_s,player_r)
 
     pygame.display.update()
     clock.tick(60)
