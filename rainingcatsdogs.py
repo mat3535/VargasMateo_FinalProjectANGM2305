54966809d3dad269cf02118b9e8e5c9e20d17811
mat3535 <mat.a.vargas35@gmail.com>
2025-04-26 20:58:14 -0500

    Updated rainingdogscats.py to receive mouse clicks and control Player movement

--- rainingcatsdogs.py
+++ rainingcatsdogs.py
@@ -11,6 +11,12 @@ clock = pygame.time.Clock()
 top = 0
 ground = 700
 
+# Column positions
+pos1 = 200
+pos2 = 500
+pos3 = 800
+pos_list = [pos1,pos2,pos3]
+
 # game on/off
 rungame = True
 
@@ -21,7 +27,7 @@ back_s = pygame.image.load('graphics/top.png').convert()
 
 # Player  graphics
 player_s = pygame.image.load('graphics/Player/stand_boy.png').convert_alpha()
-player_r = player_s.get_rect(midbottom = (500,ground))
+player_r = player_s.get_rect(midbottom = (pos2,ground))
 
 
 # Game loop
@@ -37,7 +43,17 @@ while True:
         screen.blit(ground_s,(0,ground))
         screen.blit(back_s,(0,0))
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            (xcord, ycord) = pygame.mouse.get_pos()
+            if xcord >=50 and xcord <= 350:
+                player_r.midbottom = (pos1,ground)
+            if xcord >= 351 and xcord <= 699:
+                player_r.midbottom = (pos2,ground)
+            if xcord >=700 and xcord <=950:
+                player_r.midbottom = (pos3,ground)
+
         screen.blit(player_s,player_r)
 
     pygame.display.update()
     clock.tick(60)
+
