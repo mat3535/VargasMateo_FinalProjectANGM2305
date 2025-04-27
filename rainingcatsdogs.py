6eb34e71612bf6587a2dfa06eac6ca5f63b75910
mat3535 <mat.a.vargas35@gmail.com>
2025-04-27 00:07:26 -0500

    Update rainingcatsdogs.py

--- rainingcatsdogs.py
+++ rainingcatsdogs.py
@@ -26,8 +26,17 @@ ground_r = ground_s.get_rect(midtop = (500,550))
 back_s = pygame.image.load('graphics/top.png').convert()
 
 # Player  graphics
-player_s = pygame.image.load('graphics/Player/stand_boy.png').convert_alpha()
-player_r = player_s.get_rect(midbottom = (pos2,ground))
+player1_s = pygame.image.load('graphics/Player/stand_boy.png').convert_alpha()
+player_r = player1_s.get_rect(midbottom = (pos2,ground))
+player2_s = pygame.image.load('graphics/Player/squat_boy.png').convert_alpha()
+player_frames = [player1_s, player2_s]
+player_index = 0
+player_surface = player_frames[player_index]
+
+
+# Timers
+player_animation = pygame.USEREVENT + 1
+pygame.time.set_timer(player_animation,300)
 
 
 # Game loop
@@ -43,6 +52,7 @@ while True:
         screen.blit(ground_s,(0,ground))
         screen.blit(back_s,(0,0))
 
+        # Mouse clicks for player position control / game restart
         if event.type == pygame.MOUSEBUTTONDOWN:
             (xcord, ycord) = pygame.mouse.get_pos()
             if xcord >=50 and xcord <= 350:
@@ -52,8 +62,14 @@ while True:
             if xcord >=700 and xcord <=950:
                 player_r.midbottom = (pos3,ground)
 
+        # Animate and show player
+        if event.type == player_animation:
+            if player_index == 0: 
+                player_index = 1
+            else: 
+                player_index = 0
+        player_s = player_frames[player_index]
         screen.blit(player_s,player_r)
 
     pygame.display.update()
     clock.tick(60)
-
