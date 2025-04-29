95b9ef13d391a4555650885adfccab31dd28751e
mat3535 <mat.a.vargas35@gmail.com>
2025-04-29 00:10:59 -0500

    Updated rainingcatsdogs.py, included animal art, created surfaces and rectangles, added logic for randomizing cats and dogs, added overly rapid animal rain rate, will reduce

--- rainingcatsdogs.py
+++ rainingcatsdogs.py
@@ -1,10 +1,47 @@
 import pygame
 from sys import exit
+from random import randint,choice
+
+import pygame.locals
 
 
 pygame.init()
 screen =  pygame.display.set_mode((1000,800))
+pygame.display.set_caption("It's Raining Cats and Dogs")
 clock = pygame.time.Clock()
+
+def animal_rain(animal_r_list,dog_index,cat_index):
+    """
+        Controls which animal is displayed from 
+        the list of rectangles and varies the drop rate.
+
+        animal_r_list - a list of rectangles, each at a random x-coordinate
+        from the three colum positions available.
+        The y-coordinate is a random height above the screen within
+        a range, to add variety to the drop rate.
+        
+        dog_index - a value to select which dog surface to load (for animation)
+        
+        cat_index - a value to select which cat surface to load (for animation)
+        
+        The dog's rectangle size is 1 pixel larger to differentiate it from the cat.
+
+        returns animal_r_list
+    """
+    if animal_r_list:
+        print(f'animal_r_list={animal_r_list}')
+        for animal_r in animal_r_list:
+            animal_r.y += 5
+            if animal_r.height == 201:
+                screen.blit(dog_frames[dog_index],animal_r)
+            else:
+                screen.blit(cat_frames[cat_index],animal_r)
+        
+        animal_r_list = [animal_r for animal_r in animal_r_list if animal_r.midbottom[1] < (ground + 1)]
+        
+        return animal_r_list
+    else:
+        return []
 
 # Control variables
 # Top and ground
@@ -18,7 +55,15 @@ pos3 = 800
 pos_list = [pos1,pos2,pos3]
 
 # game on/off
+global rungame
 rungame = True
+
+# Animal drop position range boundaries
+neg_top_r1 = -300
+neg_top_r2 = -500
+
+# Animal list for rain
+animal_r_list=[]
 
 # Background graphics
 ground_s = pygame.image.load('graphics/floor.png').convert()
@@ -33,10 +78,32 @@ player_frames = [player1_s, player2_s]
 player_index = 0
 player_surface = player_frames[player_index]
 
+# Cat graphics
+cat1_s = pygame.image.load('graphics/Player/cat_wag1.png').convert_alpha()
+cat_r = cat1_s.get_rect(midbottom = (pos1,top))
+cat2_s = pygame.image.load('graphics/Player/cat_wag2.png').convert_alpha()
+cat_frames = [cat1_s,cat2_s]
+cat_index = 0
+cat_surface = cat_frames[cat_index]
+
+# Dog graphics
+dog1_s = pygame.image.load('graphics/Player/dog_wag1.png').convert_alpha()
+dog_r = dog1_s.get_rect(midbottom = (pos2,top))
+dog2_s = pygame.image.load('graphics/Player/dog_wag2.png').convert_alpha()
+dog_frames = [dog1_s,dog2_s]
+dog_index = 0
+dog_surface = dog_frames[dog_index]
+
 
 # Timers
-player_animation = pygame.USEREVENT + 1
-pygame.time.set_timer(player_animation,300)
+rain_animation = pygame.USEREVENT + 1
+pygame.time.set_timer(rain_animation,300)
+
+player_animation = pygame.USEREVENT + 2
+pygame.time.set_timer(player_animation,200)
+
+animal_animation = pygame.USEREVENT + 3
+pygame.time.set_timer(animal_animation,300)
 
 
 # Game loop
@@ -61,6 +128,17 @@ while True:
                 player_r.midbottom = (pos2,ground)
             if xcord >=700 and xcord <=950:
                 player_r.midbottom = (pos3,ground)
+ 
+
+        # Append either a cat or a dog to the list 
+        # of cats and dog rectangles.
+        if event.type == rain_animation:
+            animal_position = choice(pos_list)
+            if randint(0,2):
+                animal_r_list.append(dog1_s.get_rect(midbottom = ((animal_position),randint(neg_top_r2,neg_top_r1))))
+            else:
+                animal_r_list.append(cat1_s.get_rect(midbottom = ((animal_position),randint(neg_top_r2,neg_top_r1))))
+            loop_start_time = pygame.time.get_ticks()
 
         # Animate and show player
         if event.type == player_animation:
@@ -70,6 +148,20 @@ while True:
                 player_index = 0
         player_s = player_frames[player_index]
         screen.blit(player_s,player_r)
+        
+        # Alternate the animal surface shown to create animation
+        if event.type == animal_animation:
+            if dog_index == 0:
+                dog_index = 1
+            else:
+                dog_index = 0
+            if cat_index == 0:
+                cat_index = 1
+            else:
+                cat_index = 0
+
+        # Make it rain cats and dogs
+        animal_r_list = animal_rain(animal_r_list,dog_index,cat_index)
 
     pygame.display.update()
     clock.tick(60)
